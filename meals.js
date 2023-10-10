9a00ee2a9c0889b0843bffc5e02a5d9937026e5b
DidasMba <daniel.mbarushi@gmail.com>
2023-10-10 19:53:11 +0300

    trying

--- meals.js
+++ meals.js
@@ -1,1 +1,29 @@
-<p>essaie</p>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MealsIngredient</title>
+</head>
+<body>
+    <header>
+        <h1>Meals</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Acerca de</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Contenido principal</h2>
+        <p>Este es el contenido principal de tu página.</p>
+    </main>
+
+    <footer>
+        <p>Derechos de autor &copy; 2023 Tu Nombre</p>
+    </footer>
+</body>
+</html>
