1933f6497baa8039b9a6bbc25d3bb7995b3827a2
DidasMba <daniel.mbarushi@gmail.com>
2023-10-11 23:20:22 +0300

    nav edit

--- meals.js
+++ meals.js
@@ -1,77 +1,77 @@
-let p = 1;
-let paginationEl = document.getElementById("pagination");
+// Récupérez les éléments du DOM dont vous avez besoin
+const input = document.querySelector('.input');
+const main = document.querySelector('main');
+const ingredients = document.querySelector('.ingredients');
+const apiUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
+// Écoutez l'événement de soumission du formulaire
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const searchTerm = input.value;
 
-//the both buttons
-let pBtns = paginationEl.getElementsByTagName("button");
+  // Effacez le contenu précédent
+  main.innerHTML = '';
+  ingredients.innerHTML = '';
 
-pBtns[0].addEventListener('click', goBack);
-pBtns[1].addEventListener('click', goNext);
+  // Faites une requête AJAX pour obtenir les informations sur le repas
+  fetch(apiUrl + searchTerm)
+    .then(response => response.json())
+    .then(data => {
+      const meals = data.meals;
 
-function goBack(){
-   console.log("Back");
-   if(p == 1){
-    return; // we can use return to exit a function
-   }
-   p = p - 1;
-   PageUpdate();
+      if (meals) {
+        meals.forEach(meal => {
+          // Créez les éléments HTML pour afficher les informations du repas
+          const mealDiv = document.createElement('div');
+          mealDiv.className = 'container';
 
-}
+          const mealImg = document.createElement('div');
+          mealImg.className = 'img';
+          mealImg.style.backgroundImage = `url(${meal.strMealThumb})`;
 
-function goNext(){
-    console.log("Next")
-    p = p + 1;
+          const contentContainer = document.createElement('div');
+          contentContainer.className = 'content-container';
 
-    PageUpdate();
-    getFood();
-}
+          const title = document.createElement('h1');
+          title.className = 'title';
+          title.textContent = meal.strMeal;
 
+          const info = document.createElement('p');
+          info.className = 'info';
+          info.textContent = meal.strInstructions;
 
-// uptating the current function
-function PageUpdate(){
+          const ingredientsList = document.createElement('ul');
+          ingredientsList.className = 'ingredients-list';
 
-    let span=paginationEl.getElementsByTagName("span")[0];
-    span.innerText = p;
-}
+          // Affichez les ingrédients
+          for (let i = 1; i <= 20; i++) {
+            const ingredient = meal['strIngredient' + i];
+            const measure = meal['strMeasure' + i];
+            if (ingredient && measure) {
+              const listItem = document.createElement('li');
+              listItem.textContent = `${measure} ${ingredient}`;
+              ingredientsList.appendChild(listItem);
+            }
+          }
 
+          // Ajoutez les éléments au DOM
+          contentContainer.appendChild(title);
+          contentContainer.appendChild(info);
+          contentContainer.appendChild(ingredientsList);
 
-function displayFood(data){
+          mealDiv.appendChild(mealImg);
+          mealDiv.appendChild(contentContainer);
+          main.appendChild(mealDiv);
+        });
+      } else {
+        main.innerHTML = 'Aucun repas trouvé.';
+      }
+    })
+    .catch(error => console.error(error));
+});
 
-    let divEl=document.getElementById("all-food");
-
-    let foods = "";
-
-    for (let i=0; i<data.length;i++){
-        let food = data[i];
-        foods = 
-        foods + 
-        `div class="a-food">
-
-        <div>
-            <img src="${food.image_url}"/>
-
-            <div>Name : <span>${food.name}</span></div>
-            <button>More</button>
-        </div>
-    </div>`;
-
-    }
-    divEl.innerHTML=foods;
-}
-
-function getFood(){
-
-    fetch(
-        `www.themealdb.com/api/json/v1/1/categories.php`,
-        {
-            method:"GET",
-            contentType:"application/json",
-        })
-        .then((response) => response.json()) 
-        .then(function(data) {
-            console.log(data);
-            displayFood(data.data); // Correction 2 : Utilisez data.categories pour accéder aux catégories
-        })
-
-        
-}
+// Supprimez le contenu lorsqu'une nouvelle recherche est effectuée
+input.addEventListener('focus', function () {
+  main.innerHTML = '';
+  ingredients.innerHTML = '';
+});
