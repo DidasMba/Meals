e16b55f319501b828ed2a76230c26a0dbe2fd1b5
DidasMba <daniel.mbarushi@gmail.com>
2023-10-11 16:19:36 +0300

    edit button in js

--- meals.js
+++ meals.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+let p = 1;
+let paginationEl = document.getElementById("pagination");
+
+
+//the both buttons
+let pBtns = paginationEl.getElementsByTagName("button");
+
+pBtns[0].addEventListener('click', goBack);
+pBtns[1].addEventListener('click', goNext);
+
+function goBack(){
+   console.log("Back");
+}
+
+function goNext(){
+    console.log("Next")
+}
