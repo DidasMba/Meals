accca097dbfb90be99f1c2b8b360ea4d362a3580
DidasMba <daniel.mbarushi@gmail.com>
2023-10-11 20:01:09 +0300

    error

--- meals.js
+++ meals.js
@@ -11,7 +11,7 @@ pBtns[1].addEventListener('click', goNext);
 function goBack(){
    console.log("Back");
    if(p == 1){
-    return;
+    return; // we can use return to exit a function
    }
    p = p - 1;
    PageUpdate();
@@ -23,6 +23,7 @@ function goNext(){
     p = p + 1;
 
     PageUpdate();
+    getFood();
 }
 
 
@@ -31,4 +32,46 @@ function PageUpdate(){
 
     let span=paginationEl.getElementsByTagName("span")[0];
     span.innerText = p;
+}
+
+
+function displayFood(data){
+
+    let divEl=document.getElementById("all-food");
+
+    let foods = "";
+
+    for (let i=0; i<data.length;i++){
+        let food = data[i];
+        foods = 
+        foods + 
+        `div class="a-food">
+
+        <div>
+            <img src="${food.image_url}"/>
+
+            <div>Name : <span>${food.name}</span></div>
+            <button>More</button>
+        </div>
+    </div>`;
+
+    }
+    divEl.innerHTML=foods;
+}
+
+function getFood(){
+
+    fetch(
+        `www.themealdb.com/api/json/v1/1/categories.php`,
+        {
+            method:"GET",
+            contentType:"application/json",
+        })
+        .then((response) => response.json()) 
+        .then(function(data) {
+            console.log(data);
+            displayFood(data.data); // Correction 2 : Utilisez data.categories pour accéder aux catégories
+        })
+
+        
 }
